05a5ec0f8d67c3945abf0c3ec2167161d35ab356
Craig Smith <craigjsmith99@gmail.com>
2020-07-27 14:03:57 -0400

    Initial Commit

---
+++ tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipsy
+//
+//  Created by Craig Smith on 7/27/20.
+//  Copyright © 2020 Craig Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
